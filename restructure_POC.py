b8522acd4fb45a1f1f9e4938307e70183f79f037
Michael Fessenden <MikeFez@gmail.com>
2019-10-30 21:24:07 -0400

    Added delay predictions

--- restructure_POC.py
+++ restructure_POC.py
@@ -6,19 +6,22 @@ import json
 route = "CR-Newburyport"
 departure = "Montserrat"
 arrival = "Lynn"
-api_url = f"https://api-v3.mbta.com/schedules?sort=arrival_time&include=stop%2Ctrip%2Cprediction%2Croute&filter%5Broute%5D={route}"
+api_url = f"https://api-v3.mbta.com/schedules?sort=arrival_time&include=stop%2Ctrip%2Cprediction&filter%5Broute%5D={route}"
+
+# Compare to this URL to ensure that results are the same regardless of if places are defined
+# api_url = "https://api-v3.mbta.com/schedules?sort=arrival_time&include=stop%2Ctrip%2Cprediction&filter%5Broute%5D=CR-Newburyport&filter%5Bstop%5D=place-GB-0198%2Cplace-ER-0115"
 
 
 def main():
     # Final parsed data will go in here
-    organized = {"arrival->departure": [], "departure->arrival": []}
+    organized = {"departure->arrival": [], "arrival->departure": []}
 
     res = requests.get(api_url)
     res.raise_for_status()
-
+    res_json = res.json()
     # Identify trips that contain at least a single designated stop
     departure_arrival_by_trip = {}
-    for item in res.json()['data']:
+    for item in res_json['data']:
         stop_name = item['relationships']['stop']['data']['id']
         trip_name = item['relationships']['trip']['data']['id']
         if stop_name in [arrival, departure]:
@@ -44,10 +47,35 @@ def main():
             if arrival_time >= now:
                 organized[direction].append(stops)
 
-    for direction, trips in organized.items():
-        print(direction)
-        for trip in trips:
-            print("\t", trip)
+    # Finally, lets get predictions
+    predictions_by_id = {}
+    for item in res_json['included']:
+        if item["type"] == "prediction" and item['relationships']['stop']['data']['id'] in [arrival, departure]:
+            predictions_by_id[item["id"]] = item
+
+    for direction_string, trips in organized.items():
+        print(direction_string)
+        stop_order = [arrival, departure] if direction_string == "arrival->departure" else [departure, arrival]
+        for trip_no, trip in enumerate(trips):
+            print(f"\tNext #{trip_no}")
+            for stop_no, stop_name in enumerate(stop_order):
+                print(f"\t\t{'On' if stop_no==0 else 'Off'} at [{stop_name}]: {trip[stop_name]}")
+                prediction_data = trip[stop_name]['relationships']['prediction']['data']
+                if prediction_data is not None:
+                    print(f"\t\t\tRelated Prediction: {predictions_by_id[prediction_data['id']]}")
+                    predicted_arrival = datetime.datetime.strptime(predictions_by_id[prediction_data['id']]['attributes']['arrival_time'], '%Y-%m-%dT%H:%M:%S-04:00')
+                    scheduled_arrival = datetime.datetime.strptime(trip[stop_name]["attributes"]["arrival_time"], '%Y-%m-%dT%H:%M:%S-04:00')
+                    diff = predicted_arrival - scheduled_arrival
+                    days, seconds = diff.days, diff.seconds
+                    hours = days * 24 + seconds // 3600
+                    minutes = (seconds % 3600) // 60
+                    seconds = seconds % 60
+                    print(f"\t\t\tDelayed By[H:M:S]: {hours}:{minutes}:{seconds}")
+        print("\n")
+
+    print("\nAll Predictions - there may be more here because both stops may not be on the trip:")
+    for prediction_id, prediction in predictions_by_id.items():
+        print(prediction_id, prediction)
     return
 
 
