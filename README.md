1ee9e61095975522ce0e05964f78af7c2451276c
Michael Fessenden <MikeFez@gmail.com>
2019-11-01 01:23:44 -0400

    Refactor

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # mbta_predictions
 Home Assistant custom component for MBTA predictions. Adding this custom component allows adding sensors for specific routes to Home Assistant. After the sensors are set up, they can be visualised in LoveLace using the custom [mbta-card](https://github.com/dhanani94/mbta-card).
 
-## Installation 
-The custom component source code is located [here](/custom_component/mbta_predictions). Copying the `custom_component` directory into the root directory (alongside the `configuration.yml` file) for users using HASSIO. 
+## TODO:
+* Commuter rail is DONE! But we need to figure out best methods to parse subway, hopefully with the same API call.
 
-## Configuration 
+
+## Installation
+The custom component source code is located [here](/custom_component/mbta_predictions). Copying the `custom_component` directory into the root directory (alongside the `configuration.yml` file) for users using HASSIO.
+
+## Configuration
 To enable this sensor, add the following lines to your `configuration.yaml` file:
 
 ```yaml
@@ -23,304 +27,41 @@ example `configuration.yml`:
 sensor:
   - platform: mbta_predictions
     predictions:
-     - stop: JFK/UMass
-       destination: Alewife
+     - depart_from: JFK/UMass
+       arrive_at: Alewife
        route: Red
-     - stop: Savin Hill
-       destination: Alewife
-       route: Red
-     - stop: South Station
-       destination: Ashmont
-       route: Red
+     - depart_from: Montserrat
+       arrive_at: North Station
+       route: CR-Newburyport
+       return_trips: True
+     - depart_from: Montserrat
+       arrive_at: North Station
+       route: CR-Newburyport
+       return_trips: True
 ```
 
 ### Configuration Variables
-#### stop
-> (string) (Required) the stop name (e.g. Harvard)
-#### destination
-> (string) (Required) the file stop for the train (e.g. `Braintree`)
+#### depart_from
+> (string) (Required) the stop in which you will begin transit from (e.g. Harvard)
+#### arrive_at
+> (string) (Required) the stop in which you will end your trip  (e.g. `Braintree`)
 #### route
-> (string) (Required) the route (e.g. `Red`)
+> (string) (Required) the route (e.g. `Red`, `CR-Newburyport`, `Green`)
 #### name
 > (string) (Optional) the name of the sensor (default: "mbta_STOP_NAME")
+#### return_trips
+> (boolean) (Optional) when true, will generate a second entity with depart_from/arrive_at swapped (default: False)
 #### offset_minutes
 > (int) (Optional) the minimum minutes remaining before arrival (default: 0)
 #### limit
 > (int) (Optional) the maximum number of predictions to send back (default: 10)
 
 
-### Supported Stops on the `Red` line
-
-* **Alewife**
-* Davis
-* Porter
-* Harvard
-* Central
-* Kendall/MIT
-* Charles/MGH
-* Park Street
-* Downtown Crossing
-* South Station
-* Broadway
-* Andrew
-* JFK/UMass
-* Savin Hill
-* Fields Corner
-* Shawmut
-* **Ashmont**
-* North Quincy
-* Wollaston
-* Quincy Center
-* Quincy Adams
-* **Braintree**
-
-### Supported Stops on the `Orange` line
-
-* **Oak Grove**
-* Malden Center
-* Wellington
-* Assembly
-* Sullivan Square
-* Community College
-* North Station
-* Haymarket
-* State
-* Downtown Crossing
-* Chinatown
-* Tufts Medical Center
-* Back Bay
-* Massachusetts Avenue
-* Ruggles
-* Roxbury Crossing
-* Jackson Square
-* Stony Brook
-* Green Street
-* **Forest Hills**
-
-### Supported Stops on the `Blue` line
-
-* **Wonderland**
-* Revere Beach
-* Beachmont
-* Suffolk Downs
-* Orient Heights
-* Wood Island
-* Airport
-* Maverick
-* Aquarium
-* State
-* Government Center
-* **Bowdoin**
-
-## Commuter Rail
-
-### Supported Stops on the `Fairmount` line
-
-* Blue Hill Avenue
-* Dedham Corp Center
-* Fairmount
-* Four Corners/Geneva
-* Foxboro
-* Morton Street
-* Newmarket
-* Readville
-* South Station
-* Talbot Avenue
-* Uphams Corner
-
-### Supported Stops on the `Fitchburg` line
-
-* Ayer
-* Belmont
-* Brandeis/Roberts
-* Concord
-* Fitchburg
-* Hastings
-* Kendal Green
-* Lincoln
-* Littleton/Rte 495
-* North Leominster
-* North Station
-* Porter
-* Shirley
-* Silver Hill
-* South Acton
-* Wachusett
-* Waltham
-* Waverley
-* West Concord
-
-### Supported Stops on the `Framingham/Worcester` line
-
-* Ashland
-* Auburndale
-* Back Bay
-* Boston Landing
-* Framingham
-* Grafton
-* Lansdowne
-* Natick Center
-* Newtonville
-* South Station
-* Southborough
-* Wellesley Farms
-* Wellesley Hills
-* Wellesley Square
-* West Natick
-* West Newton
-* Westborough
-* Worcester
-
-### Supported Stops on the `Franklin/Foxboro` line
-
-* Back Bay
-* Dedham Corp Center
-* Endicott
-* Forge Park/495
-* Foxboro
-* Franklin
-* Hyde Park
-* Islington
-* Norfolk
-* Norwood Central
-* Norwood Depot
-* Plimptonville
-* Readville
-* Ruggles
-* South Station
-* Walpole
-* Windsor Gardens
-
-### Supported Stops on the `Greenbush` line
-
-* Cohasset
-* East Weymouth
-* Greenbush
-* JFK/UMass
-* Nantasket Junction
-* North Scituate
-* Quincy Center
-* South Station
-* West Hingham
-* Weymouth Landing/East Braintree
-
-### Supported Stops on the `Haverhill` line
-
-* Andover
-* Ballardvale
-* Bradford
-* Greenwood
-* Haverhill
-* Lawrence
-* Malden Center
-* Melrose Cedar Park
-* Melrose Highlands
-* North Station
-* North Wilmington
-* Reading
-* Wakefield
-* Wyoming Hill
-
-### Supported Stops on the `Kingston/Plymouth` line
-
-* Abington
-* Braintree
-* Halifax
-* Hanson
-* JFK/UMass
-* Kingston
-* Plymouth
-* Quincy Center
-* South Station
-* South Weymouth
-* Whitman
-
-### Supported Stops on the `Lowell` line
-
-* Anderson/Woburn
-* Lowell
-* Mishawum
-* North Billerica
-* North Station
-* Wedgemere
-* West Medford
-* Wilmington
-* Winchester Center
-
-### Supported Stops on the `Middleborough/Lakeville` line
-
-* Braintree
-* Bridgewater
-* Brockton
-* Campello
-* Holbrook/Randolph
-* JFK/UMass
-* Middleborough/Lakeville
-* Montello
-* Quincy Center
-* South Station
-
-### Supported Stops on the `Needham` line
-
-* Back Bay
-* Bellevue
-* Forest Hills
-* Hersey
-* Highland
-* Needham Center
-* Needham Heights
-* Needham Junction
-* Roslindale Village
-* Ruggles
-* South Station
-* West Roxbury
-
-### Supported Stops on the `Newburyport/Rockport` line
-
-* Beverly
-* Beverly Farms
-* Chelsea
-* Gloucester
-* Hamilton/Wenham
-* Ipswich
-* Lynn
-* Manchester
-* Montserrat
-* Newburyport
-* North Beverly
-* North Station
-* Prides Crossing
-* River Works
-* Rockport
-* Rowley
-* Salem
-* Swampscott
-* West Gloucester
-
-### Supported Stops on the `Providence/Stoughton` line
-
-* Attleboro
-* Back Bay
-* Canton Center
-* Canton Junction
-* Hyde Park
-* Mansfield
-* Providence
-* Route 128
-* Ruggles
-* Sharon
-* South Attleboro
-* South Station
-* Stoughton
-* TF Green Airport
-* Wickford Junction
-
-## Future Plans 
-
+## Future Plans
 * Add support for the other lines (green and silver)
-* Add support for buses 
+* Add support for buses
 * Create PR for custom component in the [Home Asistant Repo](https://github.com/home-assistant/home-assistant/tree/dev/homeassistant/components)
-* Create custom compnent in HACS 
+* Create custom component in HACS
 * [Buy me coffee?](https://www.buymeacoffee.com/dhanani94)
 
 ## Inspirations
--- custom_component/mbta_predictions/sensor.py
+++ custom_component/mbta_predictions/sensor.py
@@ -13,224 +13,22 @@ from homeassistant.helpers.entity import Entity
 _LOGGER = logging.getLogger(__name__)
 
 CONF_PREDICTIONS = "predictions"
-CONF_STOP = "stop"
-CONF_DIRECTION = "destination"
+CONF_DEPART_FROM = "depart_from"
+CONF_ARRIVE_AT = "arrive_at"
 CONF_ROUTE = "route"
+CONF_RETURN_TRIPS = "return_trips"
 CONF_TIME_OFFSET = "offset_minutes"
 CONF_LIMIT = "limit"
-
-MBTA_COMMUTER_RAIL_LOOKUP_DICT = {
-    "FAIRMOUNT": "CR-Fairmount",
-    "FITCHBURG": "CR-Fitchburg",
-    "FOXBOROEVENTSERVICE": "CR-Foxboro",
-    "FRAMINGHAMWORCESTER": "CR-Worcester",
-    "FRANKLINFOXBORO": "CR-Franklin",
-    "GREENBUSH": "CR-Greenbush",
-    "HAVERHILL": "CR-Haverhill",
-    "KINGSTONPLYMOUTH": "CR-Kingston",
-    "LOWELL": "CR-Lowell",
-    "MIDDLEBOROUGHLAKEVILLE": "CR-Middleborough",
-    "NEEDHAM": "CR-Needham",
-    "NEWBURYPORTROCKPORT": "CR-Newburyport",
-    "PROVIDENCESTOUGHTON": "CR-Providence"
-}
-
-MBTA_STOP_LOOKUP_DICT = {
-    "ABINGTON": "place-PB-0194",
-    "AIRPORT": "place-aport",
-    "ALEWIFE": "place-alfcl",
-    "ANDERSONWOBURN": "place-NHRML-0127",
-    "ANDOVER": "place-WR-0228",
-    "ANDREW": "place-andrw",
-    "AQUARIUM": "place-aqucl",
-    "ASHLAND": "place-WML-0252",
-    "ASHMONT": "place-asmnl",
-    "ASSEMBLY": "place-astao",
-    "ATTLEBORO": "place-NEC-1969",
-    "AUBURNDALE": "place-WML-0102",
-    "AYER": "place-FR-0361",
-    "BACKBAY": "place-bbsta",
-    "BALLARDVALE": "place-WR-0205",
-    "BEACHMONT": "place-bmmnl",
-    "BELLEVUE": "place-NB-0072",
-    "BELMONT": "place-FR-0064",
-    "BEVERLY": "place-ER-0183",
-    "BEVERLYFARMS": "place-GB-0229",
-    "BLUEHILLAVENUE": "place-DB-2222",
-    "BOSTONLANDING": "place-WML-0035",
-    "BOWDOIN": "place-bomnl",
-    "BRADFORD": "place-WR-0325",
-    "BRAINTREE": "place-brntn",
-    "BRANDEISROBERTS": "place-FR-0115",
-    "BRIDGEWATER": "place-MM-0277",
-    "BROADWAY": "place-brdwy",
-    "BROCKTON": "place-MM-0200",
-    "CAMPELLO": "place-MM-0219",
-    "CANTONCENTER": "place-SB-0156",
-    "CANTONJUNCTION": "place-NEC-2139",
-    "CENTRAL": "place-cntsq",
-    "CHARLESMGH": "place-chmnl",
-    "CHELSEA": "place-ER-0046",
-    "CHINATOWN": "place-chncl",
-    "COHASSET": "place-GRB-0199",
-    "COMMUNITYCOLLEGE": "place-ccmnl",
-    "CONCORD": "place-FR-0201",
-    "DAVIS": "place-davis",
-    "DEDHAMCORPCENTER": "place-FB-0118",
-    "DOWNTOWNCROSSING": "place-dwnxg",
-    "EASTWEYMOUTH": "place-GRB-0146",
-    "ENDICOTT": "place-FB-0109",
-    "FAIRMOUNT": "place-DB-2205",
-    "FIELDSCORNER": "place-fldcr",
-    "FITCHBURG": "place-FR-0494",
-    "FORESTHILLS": "place-forhl",
-    "FORGEPARK495": "place-FB-0303",
-    "FOURCORNERSGENEVA": "place-DB-2249",
-    "FOXBORO": "place-FS-0049",
-    "FRAMINGHAM": "place-WML-0214",
-    "FRANKLIN": "place-FB-0275",
-    "GLOUCESTER": "place-GB-0316",
-    "GOVERNMENTCENTER": "place-gover",
-    "GRAFTON": "place-WML-0364",
-    "GREENBUSH": "place-GRB-0276",
-    "GREENSTREET": "place-grnst",
-    "GREENWOOD": "place-WR-0085",
-    "HALIFAX": "place-PB-0281",
-    "HAMILTONWENHAM": "place-ER-0227",
-    "HANSON": "place-PB-0245",
-    "HARVARD": "place-harsq",
-    "HASTINGS": "place-FR-0137",
-    "HAVERHILL": "place-WR-0329",
-    "HAYMARKET": "place-haecl",
-    "HERSEY": "place-NB-0109",
-    "HIGHLAND": "place-NB-0076",
-    "HOLBROOKRANDOLPH": "place-MM-0150",
-    "HYDEPARK": "place-NEC-2203",
-    "IPSWICH": "place-ER-0276",
-    "ISLINGTON": "place-FB-0125",
-    "JACKSONSQUARE": "place-jaksn",
-    "JFKUMASS": "place-jfk",
-    "KENDALGREEN": "place-FR-0132",
-    "KENDALLMIT": "place-knncl",
-    "KINGSTON": "place-KB-0351",
-    "LANSDOWNE": "place-WML-0025",
-    "LAWRENCE": "place-WR-0264",
-    "LINCOLN": "place-FR-0167",
-    "LITTLETONRTE495": "place-FR-0301",
-    "LOWELL": "place-NHRML-0254",
-    "LYNN": "place-ER-0115",
-    "MALDENCENTER": "place-mlmnl",
-    "MANCHESTER": "place-GB-0254",
-    "MANSFIELD": "place-NEC-2040",
-    "MASSACHUSETTSAVENUE": "place-masta",
-    "MAVERICK": "place-mvbcl",
-    "MELROSECEDARPARK": "place-WR-0067",
-    "MELROSEHIGHLANDS": "place-WR-0075",
-    "MIDDLEBOROUGHLAKEVILLE": "place-MM-0356",
-    "MISHAWUM": "place-NHRML-0116",
-    "MONTELLO": "place-MM-0186",
-    "MONTSERRAT": "place-GB-0198",
-    "MORTONSTREET": "place-DB-2230",
-    "NANTASKETJUNCTION": "place-GRB-0183",
-    "NATICKCENTER": "place-WML-0177",
-    "NEEDHAMCENTER": "place-NB-0127",
-    "NEEDHAMHEIGHTS": "place-NB-0137",
-    "NEEDHAMJUNCTION": "place-NB-0120",
-    "NEWBURYPORT": "place-ER-0362",
-    "NEWMARKET": "place-DB-2265",
-    "NEWTONVILLE": "place-WML-0081",
-    "NORFOLK": "place-FB-0230",
-    "NORTHBEVERLY": "place-ER-0208",
-    "NORTHBILLERICA": "place-NHRML-0218",
-    "NORTHLEOMINSTER": "place-FR-0451",
-    "NORTHQUINCY": "place-nqncy",
-    "NORTHSCITUATE": "place-GRB-0233",
-    "NORTHSTATION": "place-north",
-    "NORTHWILMINGTON": "place-WR-0163",
-    "NORWOODCENTRAL": "place-FB-0148",
-    "NORWOODDEPOT": "place-FB-0143",
-    "OAKGROVE": "place-ogmnl",
-    "OAKGROVEBUSWAY": "place-ogmnl",
-    "ORIENTHEIGHTS": "place-orhte",
-    "PARKSTREET": "place-pktrm",
-    "PLIMPTONVILLE": "place-FB-0177",
-    "PLYMOUTH": "place-PB-0356",
-    "PORTER": "place-portr",
-    "PRIDESCROSSING": "place-GB-0222",
-    "PROVIDENCE": "place-NEC-1851",
-    "QUINCYADAMS": "place-qamnl",
-    "QUINCYCENTER": "place-qnctr",
-    "READING": "place-WR-0120",
-    "READVILLE": "place-DB-0095",
-    "REVEREBEACH": "place-rbmnl",
-    "RIVERWORKS": "place-ER-0099",
-    "ROCKPORT": "place-GB-0353",
-    "ROSLINDALEVILLAGE": "place-NB-0064",
-    "ROUTE128": "place-NEC-2173",
-    "ROWLEY": "place-ER-0312",
-    "ROXBURYCROSSING": "place-rcmnl",
-    "RUGGLES": "place-rugg",
-    "SALEM": "place-ER-0168",
-    "SAVINHILL": "place-shmnl",
-    "SHARON": "place-NEC-2108",
-    "SHAWMUT": "place-smmnl",
-    "SHIRLEY": "place-FR-0394",
-    "SILVERHILL": "place-FR-0147",
-    "SOUTHACTON": "place-FR-0253",
-    "SOUTHATTLEBORO": "place-NEC-1919",
-    "SOUTHBOROUGH": "place-WML-0274",
-    "SOUTHSTATION": "place-sstat",
-    "SOUTHWEYMOUTH": "place-PB-0158",
-    "STATE": "place-state",
-    "STONYBROOK": "place-sbmnl",
-    "STOUGHTON": "place-SB-0189",
-    "SUFFOLKDOWNS": "place-sdmnl",
-    "SULLIVANSQUARE": "place-sull",
-    "SWAMPSCOTT": "place-ER-0128",
-    "TALBOTAVENUE": "place-DB-2240",
-    "TFGREENAIRPORT": "place-NEC-1768",
-    "TUFTSMEDICALCENTER": "place-tumnl",
-    "UPHAMSCORNER": "place-DB-2258",
-    "WACHUSETT": "place-FR-3338",
-    "WAKEFIELD": "place-WR-0099",
-    "WALPOLE": "place-FB-0191",
-    "WALTHAM": "place-FR-0098",
-    "WAVERLEY": "place-FR-0074",
-    "WEDGEMERE": "place-NHRML-0073",
-    "WELLESLEYFARMS": "place-WML-0125",
-    "WELLESLEYHILLS": "place-WML-0135",
-    "WELLESLEYSQUARE": "place-WML-0147",
-    "WELLINGTON": "place-welln",
-    "WELLINGTONSTATIONBUSWAY": "place-welln",
-    "WESTBOROUGH": "place-WML-0340",
-    "WESTCONCORD": "place-FR-0219",
-    "WESTGLOUCESTER": "place-GB-0296",
-    "WESTHINGHAM": "place-GRB-0162",
-    "WESTMEDFORD": "place-NHRML-0055",
-    "WESTNATICK": "place-WML-0199",
-    "WESTNEWTON": "place-WML-0091",
-    "WESTROXBURY": "place-NB-0080",
-    "WEYMOUTHLANDINGEASTBRAINTREE": "place-GRB-0118",
-    "WHITMAN": "place-PB-0212",
-    "WICKFORDJUNCTION": "place-NEC-1659",
-    "WILMINGTON": "place-NHRML-0152",
-    "WINCHESTERCENTER": "place-NHRML-0078",
-    "WINDSORGARDENS": "place-FB-0166",
-    "WOLLASTON": "place-wlsta",
-    "WONDERLAND": "place-wondl",
-    "WOODISLAND": "place-wimnl",
-    "WORCESTER": "place-WML-0442",
-    "WYOMINGHILL": "place-WR-0062"
-}
 
 SCAN_INTERVAL = timedelta(seconds=30)
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Optional(CONF_PREDICTIONS): [
             {
-                vol.Required(CONF_STOP): cv.string,
-                vol.Required(CONF_DIRECTION): cv.string,
+                vol.Required(CONF_DEPART_FROM): cv.string,
+                vol.Required(CONF_ARRIVE_AT): cv.string,
                 vol.Required(CONF_ROUTE): cv.string,
+                vol.Optional(CONF_RETURN_TRIPS, default=False): cv.boolean,
                 vol.Optional(CONF_TIME_OFFSET, default=0): cv.positive_int,
                 vol.Optional(CONF_LIMIT, default=10): cv.positive_int
             }
@@ -244,13 +42,17 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the MBTA sensor"""
     sensors = []
     for next_train in config.get(CONF_PREDICTIONS):
-        stop = next_train.get(CONF_STOP)
-        direction = next_train.get(CONF_DIRECTION)
+        depart_from = next_train.get(CONF_DEPART_FROM)
+        arrive_at = next_train.get(CONF_ARRIVE_AT)
         route = next_train.get(CONF_ROUTE)
         time_offset_min = next_train.get(CONF_TIME_OFFSET)
         limit = next_train.get(CONF_LIMIT)
         name = next_train.get(CONF_NAME)
-        sensors.append(MBTASensor(stop, direction, route, time_offset_min, limit, name))
+
+        sensors.append(MBTASensor(depart_from, arrive_at, route, time_offset_min, limit, name))
+        # If a return trip was specified, lets grab the reverse
+        if next_train.get(CONF_RETURN_TRIPS):
+            sensors.append(MBTASensor(arrive_at, depart_from, route, time_offset_min, limit, name))
     add_entities(sensors, True)
 
 
@@ -277,35 +79,60 @@ def convert_rel_date_to_eta_string(rd):
     return out_str.lstrip()
 
 
-# HELPER Function
-def clean_stop_string(stop):
-    stop = stop.replace(" ", "")
-    stop = stop.replace("/", "")
-    stop = stop.replace("_", "")
-    stop = stop.upper()
-    return stop
+def format_name(name):
+    return name
+
+
+def datetime_from_json(json_datatime):
+    return datetime.datetime.strptime(json_datatime, '%Y-%m-%dT%H:%M:%S-04:00')
+
+
+def get_stops_by_trip(api_response, stops_to_extract=()):
+    # Organize stops the user wants by trip. Not all trips may have both stops which we'll handle later
+    stops_by_trip = {}
+    for item in api_response['data']:
+        stop_name = item['relationships']['stop']['data']['id']
+        trip_name = item['relationships']['trip']['data']['id']
+        if not stops_to_extract or any(stop_name.lower() == stop.lower() for stop in stops_to_extract):
+            if trip_name not in stops_by_trip:
+                stops_by_trip[trip_name] = {}
+            stops_by_trip[trip_name][stop_name] = item
+    return stops_by_trip
+
+
+# def get_predictions_by_id(api_response, stops_to_extract=()):
+#     predictions_by_id = {}
+#     for item in [item for item in api_response['included'] if item["type"] == "prediction"]:
+#         stop_name = item['relationships']['stop']['data']['id']
+#         if not stops_to_extract or any(stop_name.lower() == stop.lower() for stop in stops_to_extract):
+#             predictions_by_id[item["id"]] = item
+#     return predictions_by_id
+
+
+def organize_included_data(api_response):
+    organized = {}
+    for item in api_response['included']:
+        item_type = item["type"]
+        item_id = item["type"]
+        if item_type not in organized:
+            organized[item_type] = {}
+        organized[item_type][item_id].append(item)
+    return organized
 
 
 class MBTASensor(Entity):
     """Implementation of an MBTA sensor"""
 
-    def __init__(self, stop, direction, route, time_offset_min, limit, name):
+    def __init__(self, depart_from, arrive_at, route, time_offset_min, limit, name):
         """Initialize the sensor"""
-        self._stop = stop
-        self._direction = direction
-        if clean_stop_string(route) in MBTA_COMMUTER_RAIL_LOOKUP_DICT:
-            self._route = MBTA_COMMUTER_RAIL_LOOKUP_DICT[clean_stop_string(route)]
-        else:
-            self._route = f"{route[0].upper()}{route[1:].lower()}"
+        self._depart_from = depart_from
+        self._arrive_at = arrive_at
+        self._route = route
         self._time_offset_sec = time_offset_min * 60
         self._limit = limit
-        if name:
-            self._name = name
-        else:
-            self._name = f"mbta_{self._stop}"
-        self._stop_id = MBTA_STOP_LOOKUP_DICT.get(clean_stop_string(self._stop), None)
-        self._base_url = "https://api-v3.mbta.com/predictions"
-        self._arrival_times = []
+        self._name = name if name else f"mbta_{self._depart_from}_to_{self._arrive_at}".replace(' ', '_')
+        self._base_url = "https://api-v3.mbta.com/schedules"
+        self._arrival_data = []
 
     @property
     def name(self):
@@ -314,70 +141,72 @@ class MBTASensor(Entity):
 
     @property
     def state(self):
-        """Return the next arrival times"""
-        predictions = []
-        logging.debug(f"creating predictions from {len(self._arrival_times)} arrival_times")
-        for arrival_time in self._arrival_times:
-            current_time = datetime.datetime.now()
-            if (arrival_time - current_time).total_seconds() > self._time_offset_sec:
-                rd = relativedelta(arrival_time, current_time)
-                predictions.append(convert_rel_date_to_eta_string(rd))
-            if self._limit and len(predictions) == self._limit:
-                return predictions
-
-        if len(predictions) == 0:
+        """Return the next arrival time"""
+        if len(self._arrival_data) == 0:
             logging.debug("no valid predictions, returning empty list")
-            return ["--"]
-
-        return json.dumps(predictions)
+            return "Nothing Scheduled"
+        else:
+            return self._arrival_data[0]['departure']
 
     @property
     def device_state_attributes(self):
         """Return the state attributes """
         logging.debug("returing attributes")
-        return {"route": self._route, "stop": self._stop, "direction": self._direction}
+        return {"route": self._route,
+                "depart_from": self._depart_from,
+                "arrive_at": self._arrive_at,
+                "delay": self._arrival_data[0]['delay'] if len(self._arrival_data) > 0 else None,
+                "upcoming_departures": json.dumps(self._arrival_data[1:self._limit]) if len(self._arrival_data) > 0 else json.dumps([])}
 
     def update(self):
         """Get the latest data and update the state."""
         try:
-            if self._stop_id is None:
-                raise Exception(f"invalid stop: {self._stop}")
-            url = f"{self._base_url}?include=trip&filter[route]={self._route}&filter[stop]={self._stop_id}"
-            logging.debug("attempting to update the arrival times")
-            logging.debug(f"connecting to API: {url}")
+            url = f"{self._base_url}?sort=arrival_time&include=stop%2Ctrip%2Cprediction&filter%5Broute%5D={self._route}"
+            logging.debug(f"Requesting API to update {self._route} [{self._depart_from}]: {url}")
             resp = requests.get(url)
             if resp.status_code != 200:
                 raise Exception('ERROR CODE: {}'.format(resp.status_code))
-
-            logging.debug("successfully retrieved data")
+            logging.debug(f"Successfully retrieved data for {self._route} [{self._depart_from}]")
             resp_json = resp.json()
             if not resp_json["data"]:
-                raise Exception("stop not found on route!")
+                raise Exception("Route data was not found!")
 
-            logging.debug("parsing direction IDs from API")
-            dir_to_name = {}
-            for incl in resp_json["included"]:
-                dir_id = incl['attributes']['direction_id']
-                sign = incl['attributes']['headsign']
-                if dir_id in dir_to_name:
-                    if sign.lower() in dir_to_name[dir_id].lower():
-                        pass
-                    else:
-                        dir_to_name[dir_id] = "{} or {}".format(dir_to_name[dir_id], sign)
-                else:
-                    dir_to_name[dir_id] = sign
+            # Lets grab data in organized form
+            included_data = organize_included_data(resp_json)
 
-            logging.debug("parsing arrival times")
-            self._arrival_times = []
-            for data in resp_json["data"]:
-                # departure_time = data["attributes"]["departure_time"]
-                arrival_time = data["attributes"]["arrival_time"]
-                direction_id = data["attributes"]["direction_id"]
-                direction = dir_to_name[direction_id]
-                if self._direction.lower() not in direction.lower():
-                    continue
-                if arrival_time:
-                    arrival_time = datetime.datetime.strptime(arrival_time, '%Y-%m-%dT%H:%M:%S-04:00')
-                    self._arrival_times.append(arrival_time)
+            # These don't need to be parsed as we will reference them by key
+            predictions_by_id = included_data["prediction"]
+            stop_name_by_id = {stop['attributes']['name']: stop['id'] for stop in included_data['stop']}
+
+            stops_by_trip = get_stops_by_trip(resp_json, stops_to_extract=[stop_name_by_id[self._depart_from],
+                                                                           stop_name_by_id[self._arrive_at]])
+
+            current_time = datetime.datetime.now()
+            # Now we're going to start parsing through the stops on trip and prediction data
+            self._arrival_data = []
+            for trip, stops in stops_by_trip.items():
+                # If trip has both stops AND the trip has the depart_from stop before the arrive_at stop
+                if len(stops) == 2 and stops[self._depart_from]["attributes"]["stop_sequence"] < stops[self._arrive_at]["attributes"]["stop_sequence"]:
+                    scheduled_time = datetime_from_json(stops[self._depart_from]["attributes"]["departure_time"])
+                    predicted_time = None  # Gotta figure out if predicted departure is accurate
+                    prediction_data = stops[self._depart_from]['relationships']['prediction']['data']
+                    if prediction_data is not None and prediction_data['id'] in predictions_by_id:
+                        predicted_time = datetime_from_json(predictions_by_id[prediction_data['id']]['attributes']['departure_time'])
+
+                    # Use prediction might not be available, or scheduled time if not.
+                    accurate_departure = predicted_time if predicted_time is not None else scheduled_time
+
+                    if (accurate_departure - current_time).total_seconds() > self._time_offset_sec:
+                        time_until_arrival = convert_rel_date_to_eta_string(relativedelta(accurate_departure, current_time))
+                        delay = None
+                        # If there is a prediction, and the prediction isn't the actual scheduled arrival time
+                        if predicted_time is not None and predicted_time != scheduled_time:
+                            delay = convert_rel_date_to_eta_string(relativedelta(predicted_time, scheduled_time))
+
+                        self._arrival_data.append({
+                            "departure": time_until_arrival,
+                            "delay": delay
+                        })
+
         except Exception as e:
-            logging.exception(f"not sure what must be done here: {e}")
+            logging.exception(f"Encountered Exception: {e}")
